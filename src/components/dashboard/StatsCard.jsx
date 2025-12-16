e866daa877d7a613b111b4e81e425a02e7006f69
Alfred-6578 <alfred.nwankwo719@gmail.com>
2025-12-16 10:09:41 +0100

    Style update: .stats-gradients

--- src/components/dashboard/StatsCard.jsx
+++ src/components/dashboard/StatsCard.jsx
@@ -6,7 +6,7 @@ const StatsCard = ({ icon, count, label, color }) => {
   }
   
   return (
-    <div className="flex flex-col gap-4 bg-[linear-gradient(117.58deg,rgba(215,237,237,0.16)-47.79%,rgba(204,235,235,0)100%)] rounded-lg p-6 py-8 h-[152px]">
+    <div className="flex flex-col gap-4 stats-gradient rounded-lg p-6 py-8 h-[152px]">
         <div className="flex gap-3 items-center">
             <img src={icon} alt="icon" className="w-auto h-[27px]"/>
              <h3 className="text-3xl font-bold text-white mb-1">{count}</h3>
